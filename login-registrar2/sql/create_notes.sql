42678187788397d892067b83c34b88068f85e0ed
MiguelAugustoRamos <miguel.augusto.ramos@escola.pr.gov.br>
2025-08-04 00:02:05 -0300

    1

--- login-registrar2/sql/create_notes.sql
+++
@@ -1,9 +0,0 @@
--- Criação da tabela notes
-CREATE TABLE notes (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  user_email VARCHAR(255) NOT NULL,
-  content TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_email) REFERENCES users(email) ON DELETE CASCADE
-);
-
